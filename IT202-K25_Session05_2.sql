916d6a6b6a486c0f8897dfd61686412f33f2922d
Nguyễn Hoàng Anh <143367993+dfrgtyhujio@users.noreply.github.com>
2026-04-20 22:23:00 +0700

    Add files via upload

--- IT202-K25_Session05_2.sql
+++ IT202-K25_Session05_2.sql
@@ -1,5 +1,20 @@
 -- Câu lệnh đang thiếu mệnh đề order by
 
+use db_demo;
+
+create table Restaurants (
+    restaurant_name varchar(50),
+    created_at datetime
+);
+
+insert into Restaurants values
+	('Phở Hòa', '2024-01-01 10:00:00'),
+	('Bún Bò', '2024-02-15 12:00:00'),
+	('Cơm Tấm', '2024-03-10 08:30:00'),
+	('Pizza', '2024-04-05 18:00:00'),
+	('Sushi', '2024-05-20 19:00:00'),
+	('Lẩu', '2024-06-01 20:00:00');
+
 select restaurant_name, created_at
 from Restaurants 
 order by created_at desc
